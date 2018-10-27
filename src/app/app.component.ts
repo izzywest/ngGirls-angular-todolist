c76c68abdb2e93a8c6d291164564519a44103b16
schahnaz <schahnaz@mail.de>
2018-10-27 12:39:58 +0200

    finished until #10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+
+  <app-input-button-unit></app-input-button-unit>
+
+  <ul>
+  <li *ngFor="let todoItem of todoList">
+    <app-todo-item [item]="todoItem"></app-todo-item>
+  </li>
+</ul>
+
+`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
-}
+  title = 'app';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ]};
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+  <p>
+    input-button-unit works!
+    The title is: {{ title }}
+  </p>
+`,  
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+
+
+  title: string = 'Hello World';
+
+constructor() { 
+  this.title = 'I Love Angular';
+}
+
+ngOnInit() { 
+  this.title = 'Angular CLI Rules!';
+}
+
+}
+
+
+
+
+
+
